19d0e080b001893a061dcb4f86aadd3399abf979
miguellatorre-afk <a28715@svalero.com>
2025-05-06 14:35:33 +0200

    Update consultas.sql

--- AA/consultas.sql
+++ AA/consultas.sql
@@ -11,7 +11,7 @@ WHERE NUMFACTURA = 'F0002';
 COMMIT;
 
 --3. Crear una consulta que muestre un listado agrupado de toda o parte de la información almacenada. En la consulta deben aparecen un filtrado con la función de agregado utilizada.--
-
+--SELECCIOMNAR EL TOTAL DE HORAS INVERTIDAS EN VEHICULOS QUE INGRESARON ANTES DE ABRIL DE 2024
 SELECT VE.FE_INGRESO, SUM(FA.NHORAS)
 FROM FACTURA FA INNER JOIN VEHICULOS VE
 ON FA.MATRICULA=VE.MATRICULA
@@ -19,7 +19,10 @@ GROUP BY VE.FE_INGRESO
 HAVING SUM(FA.NHORAS) < '2024-04-01';
 
 --4.Crear una consulta que muestre, mediante una combinación externa, un listado con toda la información de una tabla principal que no tenga información relacionada en una tabla secundaria.--
-
-
+--SELECCIONAR VEHICULOS CUYAS FACTURAS AUN NO HAN SIDO PAGADAS
+SELECT VE.*
+FROM VEHICULOS VE LEFT JOIN FACTURA FA
+ON VE.MATRICULA=FA.MATRICULA
+WHERE FA.FE_PAGO IS NULL;
 
 --5. Crear una consulta que muestre un listado de registros que contengan una determinada cadena en un campo de tu elección --
