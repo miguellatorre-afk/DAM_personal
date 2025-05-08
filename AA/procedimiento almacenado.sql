4c6b0ee83a5cdc65d80ef3e62bb56b02a484ed81
miguellatorre-afk <a28715@svalero.com>
2025-05-08 19:49:55 +0200

    Update procedimiento almacenado.sql

--- AA/procedimiento almacenado.sql
+++ AA/procedimiento almacenado.sql
@@ -2,36 +2,32 @@
 --El/los criterios de borrado de la tabla principal deberán obtenerse a partir de un parámetro de entrada del procedimiento almacenado.
 --BORRADO DE LOS REGISTROS DE LA TABLA DETFACTURA EN DONDE EL VALOR DE IMPORTE CORRESPONDIENTE DE LA TABLA REPUESTOS SEA SUPERIOR AL PARAMETRO DE ENTRADA
 CREATE OR REPLACE PROCEDURE BORRADO_PARCIAL(IMPORTE_MAXIMO IN NUMBER) AS
-
-
-SET SERVEROUTPUT ON
-
+    
 BEGIN
+DELETE FROM DETFACTURA 
+WHERE REFERENCIA IN (
+    SELECT REFERENCIA
+    FROM REPUESTOS
+    WHERE RE.IMPORTE>IMPORTE_MAXIMO);
 
-DELETE 
-FROM DETFACTURA DE
-INNER JOIN REPUESTOS RE
-ON DE.REFERENCIA=RE.REFERENCIA
-WHERE RE.IMPORTE>IMPORTE_MAXIMO;
+DBMS_OUTPUT.PUT_LINE(SQL%ROWCOUNT || ' filas borradas.');
 
 EXCEPTION
-
     WHEN NO_DATA_FOUND THEN
         DBMS_OUTPUT.PUT_LINE('NO SE ENCONTRÓ EL IMPORTE');
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('ERROR: ' || SQLCODE || ' : ' || SQLERRM);        
-
+        DBMS_OUTPUT.PUT_LINE('ERROR: ' || SQLCODE || ' : ' || SQLERRM);       
 END;
 /
 
 --2-Crea un procedimiento almacenado con parámetros de entrada y salida. Dicho procedimiento debe contener un cursor usado cuando se cumpla cierta condición en el
 --parámetro de entrada. El parámetro de salida deberá ser mostrado por pantalla en la llamada al procedimiento.
 
-CREATE OR REPLACE PROCEDURE COMPARATIVA(CITY IN VARCHAR(20) AS
+CREATE OR REPLACE PROCEDURE COMPARATIVA(CITY IN VARCHAR2(20), INFO_CLIEN OUT VARCHAR2) AS
 
 DECLARE
 CURSOR C1 IS SELECT NOMBRE, APELLIDOS FROM CLIENTES;
-    
+
 BEGIN
 
 OPEN C1
